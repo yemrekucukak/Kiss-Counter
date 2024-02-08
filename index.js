39ec7a86afcb9895596d9bb8ca2e6203dd22152d
yemrekucukak <91425622+yemrekucukak@users.noreply.github.com>
2024-02-08 21:57:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/api/submit', (req, res) => {
 });
 
 // Start the server on port 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
